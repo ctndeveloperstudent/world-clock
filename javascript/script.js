250cc7ff3ff5ef8339559f3d671dc18009c0440c
ctndeveloperstudent <15708@coderacademy.edu.au>
2024-07-18 21:00:50 +1000

    added moment.js and timezone and made cities date and time functional

---
+++ javascript/script.js
@@ -0,0 +1,41 @@
+function updateTime(){
+
+//Sydney AU
+let sydneyElement = document.querySelector("#sydney");
+let sydneyDateElement = sydneyElement.querySelector(".date");
+let sydneyTimeElement = sydneyElement.querySelector(".time");
+let sydneyTime = moment().tz("Australia/Sydney");
+
+sydneyDateElement.innerHTML = sydneyTime.format(
+    "MMMM Do YYYY");
+sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]");
+
+//Los Angeles USA
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment().tz("America/Los_Angeles");
+
+losAngelesDateElement.innerHTML = losAngelesTime.format(
+    "MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]");
+
+
+//Paris FR
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTime = moment().tz("Europe/Paris");
+
+parisDateElement.innerHTML = parisTime.format(
+    "MMMM Do YYYY");
+parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>]A[</small>]");
+
+
+}
+
+updateTime();
+setInterval(updateTime, 1000);
